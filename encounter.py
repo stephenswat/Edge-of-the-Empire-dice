11d533171b37ec28427aee5838aa004cad62635e
Stephen Swatman <stephenswat@gmail.com>
2015-10-26 12:46:32 +0100

    Add the file for the encounter management.

---
+++ encounter.py
@@ -0,0 +1,20 @@
+"""
+This program aims to provide a simple and efficient way to run encounters in
+the Edge of the Empire roleplaying game. Doing this from paper can be confusing
+and even frustrating at times, because there is a lot to keep track of such as
+distances, critical hits, temporary modifiers, initiative slots and more.
+
+Star Wars: Edge of the Empire is a roleplaying game by Fantasy Flight Games.
+Credits go to them for developing the dice system. Apart from that, I don't
+know anything about copyrighting, so please FFG, don't sue me.
+"""
+
+__author__     = "Stephen Swatman"
+__credits__    = ["Fantasy Flight Games"]
+__license__    = "Unlicense"
+__version__    = "0.0.1"
+__email__      = "stephenswat@gmail.com"
+
+
+if __name__ == "__main__":
+    pass
